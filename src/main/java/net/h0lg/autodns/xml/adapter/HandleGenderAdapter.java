0a9efadaed1cb568aabac27be793759c7e77ee1c
Holger Steinhauer <holger+github@willige-entwickler.de>
2016-07-06 08:23:32 +0100

    Adds tests for HandleGenderAdapter

--- src/main/java/net/h0lg/autodns/xml/adapter/HandleGenderAdapter.java
+++ src/main/java/net/h0lg/autodns/xml/adapter/HandleGenderAdapter.java
@@ -3,20 +3,22 @@ package net.h0lg.autodns.xml.adapter;
 import net.h0lg.autodns.xml.request.contact.HandleGender;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.util.Arrays;
 
 public class HandleGenderAdapter extends XmlAdapter<String, HandleGender> {
 	@Override
 	public HandleGender unmarshal(String s) throws Exception {
-		for (HandleGender gender : HandleGender.values()) {
-			if (gender.getValue().equals(s)) {
-				return gender;
-			}
-		}
-		return null;
+		return Arrays.stream(HandleGender.values())
+				.filter(hg -> hg.getValue().equalsIgnoreCase(s))
+				.findFirst()
+				.orElse(HandleGender.UNKNOWN);
 	}
 
 	@Override
 	public String marshal(HandleGender gender) throws Exception {
+		if (gender == null) {
+			return HandleGender.UNKNOWN.getValue();
+		}
 		return gender.getValue();
 	}
 }
---
+++ src/test/java/net/h0lg/autodns/xml/adapter/HandleGenderAdapterTest.java
@@ -0,0 +1,82 @@
+package net.h0lg.autodns.xml.adapter;
+
+import net.h0lg.autodns.xml.request.contact.HandleGender;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class HandleGenderAdapterTest {
+
+	private final HandleGenderAdapter genderAdapter = new HandleGenderAdapter();
+
+	@Test
+	public void unmarshal_should_returnFemale_given_f() throws Exception {
+		String given = "f";
+
+		HandleGender actualResult = genderAdapter.unmarshal(given);
+
+		assertThat(actualResult, is(HandleGender.FEMALE));
+	}
+
+	@Test
+	public void unmarshal_should_returnMale_given_m() throws Exception {
+		String given = "m";
+
+		HandleGender actualResult = genderAdapter.unmarshal(given);
+
+		assertThat(actualResult, is(HandleGender.MALE));
+	}
+
+	@Test
+	public void unmarshal_should_returnUnknown_given_null() throws Exception {
+		HandleGender actualResult = genderAdapter.unmarshal(null);
+
+		assertThat(actualResult, is(HandleGender.UNKNOWN));
+	}
+
+	@Test
+	public void unmarshal_should_returnUnknown_given_emptyString() throws Exception {
+		HandleGender actualResult = genderAdapter.unmarshal("");
+
+		assertThat(actualResult, is(HandleGender.UNKNOWN));
+	}
+
+	@Test
+	public void unmarshal_should_returnUnknown_given_randomString() throws Exception {
+		HandleGender actualResult = genderAdapter.unmarshal(genderAdapter.toString());
+
+		assertThat(actualResult, is(HandleGender.UNKNOWN));
+	}
+
+	@Test
+	public void unmarshal_should_beCaseInsensitive() throws Exception {
+		String given = "F";
+
+		HandleGender actualUpperCaseResult = genderAdapter.unmarshal(given.toUpperCase());
+		HandleGender actualLowerCaseResult = genderAdapter.unmarshal(given.toLowerCase());
+
+		assertThat(actualLowerCaseResult, is(actualUpperCaseResult));
+	}
+
+	@Test
+	public void marshal_should_returnM_givenMale() throws Exception {
+		String actualResult = genderAdapter.marshal(HandleGender.MALE);
+
+		assertThat(actualResult, is(HandleGender.MALE.getValue()));
+	}
+
+	@Test
+	public void marshal_should_returnF_givenFemale() throws Exception {
+		String actualResult = genderAdapter.marshal(HandleGender.FEMALE);
+
+		assertThat(actualResult, is(HandleGender.FEMALE.getValue()));;
+	}
+
+	@Test
+	public void marshal_should_returnUnknown_givenNull() throws Exception {
+		String actualResult = genderAdapter.marshal(null);
+
+		assertThat(actualResult, is(HandleGender.UNKNOWN.getValue()));
+	}
+}
