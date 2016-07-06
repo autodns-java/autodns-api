2d02e78f24f1745a68b2a9b9e0f808958f859dc5
Holger Steinhauer <holger+github@willige-entwickler.de>
2016-07-06 19:33:20 +0100

    Adds `HandleExtensionModeAdapter` tests.

--- src/main/java/net/h0lg/autodns/xml/adapter/HandleExtensionModeAdapter.java
+++ src/main/java/net/h0lg/autodns/xml/adapter/HandleExtensionModeAdapter.java
@@ -3,21 +3,23 @@ package net.h0lg.autodns.xml.adapter;
 import net.h0lg.autodns.xml.request.contact.HandleExtensionMode;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.util.Arrays;
 
 public class HandleExtensionModeAdapter extends XmlAdapter<String, HandleExtensionMode> {
 	@Override
 	public HandleExtensionMode unmarshal(String s) throws Exception {
-		for (HandleExtensionMode mode : HandleExtensionMode.values()) {
-			if (mode.getValue().equals(s)) {
-				return mode;
-			}
-		}
-
-		return null;
+		return Arrays.stream(HandleExtensionMode.values())
+				.filter(hem -> hem.getValue().equalsIgnoreCase(s))
+				.findFirst()
+				.orElse(null);
 	}
 
 	@Override
 	public String marshal(HandleExtensionMode mode) throws Exception {
+		if (mode == null) {
+			return null;
+		}
+
 		return mode.getValue();
 	}
 }
---
+++ src/test/java/net/h0lg/autodns/xml/adapter/HandleExtensionModeAdapterTest.java
@@ -0,0 +1,75 @@
+package net.h0lg.autodns.xml.adapter;
+
+import net.h0lg.autodns.xml.request.contact.HandleExtensionMode;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.*;
+
+public class HandleExtensionModeAdapterTest {
+
+	private HandleExtensionModeAdapter adapter = new HandleExtensionModeAdapter();
+
+	@Test
+	public void unmarshal_should_returnMerge_given_merge() throws Exception {
+		String given = "merge";
+
+		HandleExtensionMode actualResult = adapter.unmarshal(given);
+
+		assertThat(actualResult, is(HandleExtensionMode.MERGE));
+	}
+
+	@Test
+	public void unmarshal_should_returnReplace_given_replace() throws Exception {
+		String given = "replace";
+
+		HandleExtensionMode actualResult = adapter.unmarshal(given);
+
+		assertThat(actualResult, is(HandleExtensionMode.REPLACE));
+	}
+
+	@Test
+	public void unmarshal_should_beCaseInsensitive() throws Exception {
+		String given = "MeRgE";
+
+		HandleExtensionMode actualResult = adapter.unmarshal(given);
+
+		assertThat(actualResult, is(HandleExtensionMode.MERGE));
+	}
+
+	@Test
+	public void unmarshal_should_beNullSafe() throws Exception {
+		HandleExtensionMode actualResult = adapter.unmarshal(null);
+
+		assertThat(actualResult, is(nullValue()));
+	}
+
+	@Test
+	public void unmarshal_should_returnNull_given_randomValue() throws Exception {
+		HandleExtensionMode actualResult = adapter.unmarshal(adapter.toString());
+
+		assertThat(actualResult, is(nullValue()));
+	}
+
+	@Test
+	public void marshal_should_returnMerge() throws Exception {
+		String actualResult = adapter.marshal(HandleExtensionMode.MERGE);
+
+		assertThat(actualResult, is(HandleExtensionMode.MERGE.getValue()));
+	}
+
+	@Test
+	public void marshal_should_returnReplace() throws Exception {
+		String actualResult = adapter.marshal(HandleExtensionMode.REPLACE);
+
+		assertThat(actualResult, is(HandleExtensionMode.REPLACE.getValue()));
+	}
+
+	@Test
+	public void marshal_should_returnNull_given_null() throws Exception {
+		String actualResult = adapter.marshal(null);
+
+		assertThat(actualResult, is(nullValue()));
+	}
+}
