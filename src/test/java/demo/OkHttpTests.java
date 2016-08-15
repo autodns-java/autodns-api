32116fdcf7bbd5162ffa048aae727b33c032e6bd
daincredibleholg <hl+github@willige-entwickler.de>
2016-08-15 18:56:37 +0100

    Introduce OkHttp3 test and properties file
    This commit adds a dependency to OkHttp3 and a simple test class to
    play around with it a bit. At the moment it sends the Contact (aka Handle
    ) List enquiry, created via the corresponding builder, via OkHttp to the
    AutoDNS demo server.
    The authentication for this is in a properties file which needs to be in
    the root directory and must have the name "autodns.properties". An
    example of this file is also part of this commit
    (`autodns.sample.properties`) and a small addition to `.gitignore` should
    pretend you from commiting your production / real life logins to GitHub.

---
+++ src/test/java/demo/OkHttpTests.java
@@ -0,0 +1,75 @@
+package demo;
+
+import net.h0lg.autodns.xml.builder.HandleListRequestBuilder;
+import net.h0lg.autodns.xml.request.model.Auth;
+import net.h0lg.autodns.xml.request.model.Request;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.junit.Test;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Properties;
+
+public class OkHttpTests {
+
+	private static final String PASSWORD = "domain";
+	private static final String CONTEXT = "1";
+	private static final String AUTODNS_DEMO_HOST = "https://demo.autodns.com/gateway/";
+	private Properties properties;
+
+	@Test
+	public void sendAContactListEnquiry() throws JAXBException, IOException {
+		HandleListRequestBuilder listBuilder = new HandleListRequestBuilder(
+				new Auth(getUsername(), getPassword(), getContext())
+		);
+
+		StringWriter messageBody = new StringWriter();
+
+		JAXBContext context = JAXBContext.newInstance(Request.class);
+		Marshaller marshaller = context.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+		;
+		marshaller.marshal(listBuilder.build(), messageBody);
+
+		OkHttpClient client = new OkHttpClient();
+
+		RequestBody body = RequestBody.create(MediaType.parse("text/xml"), messageBody.toString());
+
+		okhttp3.Request httpRequest = new okhttp3.Request.Builder()
+				.url(AUTODNS_DEMO_HOST)
+				.post(body)
+				.build();
+
+		Response response = client.newCall(httpRequest).execute();
+
+		System.out.println("Response: [" + response.body().string() + "]");
+
+	}
+
+	private Properties getProperties() throws IOException {
+		if (this.properties == null) {
+			this.properties = new Properties();
+			this.properties.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("autodns.properties"));
+		}
+
+		return this.properties;
+	}
+
+	private String getUsername() throws IOException {
+		return getProperties().getProperty("autodns.username");
+	}
+
+	private String getPassword() throws IOException {
+		return getProperties().getProperty("autodns.password");
+	}
+
+	private String getContext() throws IOException {
+		return getProperties().getProperty("autodns.context");
+	}
+}
